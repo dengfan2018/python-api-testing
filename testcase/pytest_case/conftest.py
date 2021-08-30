1f67d1c44ed0f355ff53a8e554d9f8158f449a71
kanghe <244783726@qq.com>
2021-08-30 18:53:59 +0800

    no message

---
+++ testcase/pytest_case/conftest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/30 16:31
+# @Author  : kanghe
+# @Email   : 244783726@qq.com
+# @File    : conftest.py
---
+++ testcase/pytest_case/test_recharge.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/30 18:01
+# @Author  : kanghe
+# @Email   : 244783726@qq.com
+# @File    : test_recharge.py
